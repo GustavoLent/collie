d1b8c08eb7da268438ff8b65b48b951cb3df6bc1
Gustavo <gustavo.lent@unesp.br>
2022-07-17 23:11:09 -0300

    added front

---
+++ front/src/MenuList.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.DefaultListModel;
+import javax.swing.JList;
+import javax.swing.ListCellRenderer;
+import javax.swing.SwingUtilities;
+
+/**
+ *
+ * @author JoséRobertoFernandes
+ * @param <E>
+ */
+public class MenuList<E extends Object> extends JList<E> {
+
+    private final DefaultListModel model;
+    private int selectedIndex = -1;
+
+    private EventMenuSelected event;
+
+    public void addEventMenuSelected(EventMenuSelected event) {
+        this.event = event;
+    }
+
+    public MenuList() {
+        model = new DefaultListModel();
+        setModel(model);
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                if (SwingUtilities.isLeftMouseButton(e)) {
+                    int index = locationToIndex(e.getPoint());
+                    Object o = model.getElementAt(index);
+                    if (o instanceof MenuModel) {
+                        MenuModel menu = (MenuModel) o;
+                        if (menu.getType() == MenuModel.MenuType.MENU) {
+                            selectedIndex = index;
+                            if (event != null) {
+                                event.selected(index);
+                            }
+                        }
+                    } else {
+                        selectedIndex = index;
+                    }
+                    repaint();
+                }
+            }
+        });
+    }
+
+    @Override
+    public ListCellRenderer<? super E> getCellRenderer() {
+        return new DefaultListCellRenderer() {
+            @Override
+            public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected,
+                    boolean cellHasFocus) {
+                MenuModel data;
+                if (value instanceof MenuModel) {
+                    data = (MenuModel) value;
+                } else {
+                    data = new MenuModel("", value + "", MenuModel.MenuType.EMPTY);
+                }
+                MenuItem item = new MenuItem(data);
+                item.setSelected(selectedIndex == index);
+                return item;
+            }
+        };
+    }
+
+    public void addItem(MenuModel data) {
+        model.addElement(data);
+    }
+}
---
+++ front/src/TableStatus.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+import java.awt.Color;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author JoséRobertoFernandes
+ */
+public class TableStatus extends JLabel {
+
+    public TableStatus() {
+        setForeground(Color.WHITE);
+    }
+
+    private StatusType type;
+
+    public StatusType getType() {
+        return type;
+    }
+
+    public void setType(StatusType type) {
+        this.type = type;
+        setText(type.toString());
+        repaint();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        if (type != null) {
+            Graphics2D g2 = (Graphics2D) g;
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            GradientPaint grad;
+            if (type == StatusType.CONCLUIDO) {
+                grad = new GradientPaint(0, 0, Color.decode("#43cea2"), 0, getHeight(), Color.decode("#185a9d"));
+            } else {
+                grad = new GradientPaint(0, 0, Color.decode("#EE0979"), 0, getHeight(), Color.decode("#FF6A00"));
+            }
+            g2.setPaint(grad);
+            g2.fillRoundRect(0, 0, getWidth(), getHeight(), 10, 10);
+            // g2.fillRect(getWidth()-20, 0, getWidth(), getHeight());
+        }
+        super.paintComponent(g); // Generated from
+                                 // nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+}
