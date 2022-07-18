c76a5c48bcf349b41d8c4bfab1735b25b438ba0a
José Fernandes <jose.russino@unesp.br>
2022-07-17 22:26:47 -0300

    gui swing

---
+++ api/src/main/java/com/collie/swing/StatusType.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.collie.swing;
+
+/**
+ *
+ * @author JoséRobertoFernandes
+ */
+public enum StatusType {
+    PENDENTE, CONCLUIDO
+}
